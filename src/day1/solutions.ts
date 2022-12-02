7bba05547b1da441c472b0e24399ebb3781d2760
chadmroberts88 <chadmroberts88@gmail.com>
2022-12-02 15:23:36 -0330

    Day 1 Solutions.

--- src/day1/solutions.ts
+++ src/day1/solutions.ts
@@ -1,7 +1,32 @@
-export function solution1A() {
-  console.log("Hello from day1 partA");
+import fs from "fs";
+import path from "path";
+
+export function solution1A(): number[] {
+  const snackz = fs
+    .readFileSync(path.resolve(__dirname, "input.txt"))
+    .toString()
+    .split("\n");
+
+  const elfTotals: number[] = [];
+  let counter = 0;
+
+  for (let i = 0; i < snackz.length; i++) {
+    if (!isNaN(parseInt(snackz[i]))) {
+      if (elfTotals[counter] === undefined) elfTotals[counter] = 0;
+      elfTotals[counter] += parseInt(snackz[i]);
+    } else {
+      counter++;
+    }
+  }
+
+  console.log(Math.max(...elfTotals));
+  return elfTotals;
 }
 
-export function solution1B() {
-  console.log("Hello from day1 partB");
+export function solution1B(): void {
+  const sortedSnackz: number[] = solution1A().sort((a, b) => {
+    return b - a;
+  });
+
+  console.log(sortedSnackz[0] + sortedSnackz[1] + sortedSnackz[2]);
 }
