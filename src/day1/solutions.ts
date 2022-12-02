f00164113b7db7f26336721519d83ce181160a21
chadmroberts88 <chadmroberts88@gmail.com>
2022-12-02 13:51:08 -0330

    Initial setup.

---
+++ src/day1/solutions.ts
@@ -0,0 +1,7 @@
+export function solution1A() {
+  console.log("Hello from day1 partA");
+}
+
+export function solution1B() {
+  console.log("Hello from day1 partB");
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { solution1A, solution1B } from "./day1/solutions";
+
+solution1A();
+solution1B();
